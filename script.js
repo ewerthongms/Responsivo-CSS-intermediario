ebf65288cfdd734e9bcef9be2697b16fcf055d44
ewerthongms <ewerthongms@gmail.com>
2026-03-09 19:20:33 -0300

    JS adicionado

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const button = document.getElementById('meuBotao');
+
+button.addEventListener('click', function() {
+    alert('Botão clicado!');
+});
+
+console.log('Script carregado com sucesso!');
